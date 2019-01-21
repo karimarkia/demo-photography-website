419b0bb7fb509acc683d51e2b4b08a3f9fec70cc
karimarkia <karimarkia@gmail.com>
2019-01-21 20:56:34 +0200

    demo website

---
+++ new project/javascript/javascript.js
@@ -0,0 +1,79 @@
+$(document).ready(function(){
+
+    var $header=$('header');
+    var $sticky=$header.before($header.clone().addClass("sticky"));
+    $(window).on("scroll",function(){
+        var scrollFromTop=$(window).scrollTop();
+        $("body").toggleClass("scroll",(scrollFromTop>350));
+    });
+
+    $('.menu li a[href^="#"]').on('click', function(e){
+        e.preventDefault();
+
+        var target=$(this.hash);
+
+        if(target.length){
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top -80
+            }, 1000);
+        }
+
+    });
+
+    //Masnory
+    $('.grid').masonry({
+
+        itemSelector: '.grid-item',
+        columnWidth: 120,
+        fitWidth:true
+    });
+
+    //slick slider
+    $('.slider').slick({
+        autoplay:true,
+        autoplaySpeed:1500,
+        centerMode:true,
+        slidesToShow:3,
+        arrows:true,
+        prevArrow:'<button type="button" class="slick-prev">Previous</button>',
+        nextArrow:'<button type="button" class="slick-next">next</button>',
+
+        responsive: [
+            {
+                breakpoint:990,
+               settings: { 
+                   slidesToShow: 2 
+                    }
+            },
+            {
+                breakpoint:768,
+               settings:{
+                    slidesToShow:1 
+                 }
+            }
+        ]
+
+    });
+
+
+    //Responsive menu
+
+    var body =$('body');
+    var mainTrigger=$('.js-main-trigger');
+    var mainOverlay=$('.js-main-overlay');
+
+    mainTrigger.on('click',function(){
+        body.addClass('menu-is-active');
+    });
+    
+    mainOverlay.on('click',function(){
+        body.removeClass('menu-is-active');
+    });
+
+    $('.menu li a').on('click',function(){
+        body.removeClass('menu-is-active');
+    });
+
+
+
+});
